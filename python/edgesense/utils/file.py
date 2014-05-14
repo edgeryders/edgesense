3cb3c1feef4e83321e973411617eb0aadfcdbc38
Luca Mearelli <l.mearelli@spazidigitali.com>
2014-05-14 17:39:54 +0200

    re-organizes the code layout moves the python script to its own directory and moves the demo code to a top level directory

---
+++ python/edgesense/utils/file.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+def mkdir(newdir):
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired " \
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            mkdir(head)
+        if tail:
+            os.mkdir(newdir)
+
+def save(thing, filename, dirname='', formatted=False):
+    mkdir(dirname)
+    filename = os.path.join(dirname, filename)
+    with open(filename, 'w') as outfile:
+        if formatted:
+            json.dump(thing, outfile, indent=4, sort_keys=True)
+        else:
+            json.dump(thing, outfile)
