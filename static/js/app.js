8fcc7bb8b88ebfe5e1c9d6c164e866fed6491e45
Luca Mearelli <l.mearelli@spazidigitali.com>
2014-05-27 11:47:13 +0200

    adds contextual help support fixes #10

--- static/js/app.js
+++ static/js/app.js
@@ -81,6 +81,84 @@ jQuery(function($) {
 
     });
     
+    var Help = function(){
+
+        var base_url = '/json',
+            file = 'help.json',
+            lang = 'en',
+            data = undefined,
+            helped = undefined,
+            popovers = [],
+            make_popover = function(element, key, placement){
+              $(element).popover({
+                  placement: placement||'auto',
+                  content: data[lang][key],
+                  trigger: 'manual'
+              })  
+            },
+            activate = function(element){
+              var help_key = $(element).data('help');
+              var help_placement = $(element).data('help-placement');
+              if (help_key && data[lang][help_key]) {
+                  var help_button = $('<i class="help-button fa fa-fw fa-question-circle"></i>');
+                  $(element).append(help_button);
+                  help_button.position({
+                                  my: "right-3 top+4",
+                                  at: "right top",
+                                  of: $(element),
+                                  within: $(element)
+                              });
+                  
+                  make_popover(element,help_key, help_placement);
+                  
+                  help_button.on('click', function(){
+                      $(element).popover('toggle')
+                  });
+              }  
+            },
+            build = function(d){
+                data = d;
+                helped = $('.helped');
+                _.each(helped, activate);
+            };
+
+        function h(){
+        };
+
+        h.base = function(url){
+            if (!arguments.length) return base_url;
+
+            base_url = url;
+
+            return h;
+        };
+
+        h.file = function(filename){
+            if (!arguments.length) return file;
+
+            file = filename;
+
+            return h;
+        };
+
+        h.load = function(){
+            
+            $.ajax({
+              dataType: "json",
+              url: base_url+"/"+file, 
+              success: build
+            });
+
+            return h;
+        };
+        
+        h.data = function(){
+            return data;
+        };
+
+        return h;
+    };
+    
     var Dashboard = function(){
 
         var base_url = undefined,
@@ -665,6 +743,8 @@ jQuery(function($) {
                               .load('network.min.json');
     
           window.Dashboard.run();
+          
+          Help().load();
       }
     });
 
