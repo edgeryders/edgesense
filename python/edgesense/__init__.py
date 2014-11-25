4c5d6a17950f31c07bf1bd5a7bbd3389947a6596
Luca Mearelli <l.mearelli@spazidigitali.com>
2014-11-25 17:22:56 +0100

    makes the twitter and catalyst packages optional

--- python/edgesense/__init__.py
+++ python/edgesense/__init__.py
@@ -1,5 +1,12 @@
 import utils
 import network
 import content
-import twitter
-import catalyst
+# Optional packages
+try:
+    import twitter
+except ImportError:
+    print('twitter failed to import')
+try:
+    import catalyst
+except ImportError:
+    print('catalyst failed to import')
